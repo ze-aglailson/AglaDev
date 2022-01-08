13b9a541e320490a57b90a263642b016a9628046
zeaglailson <jaglailson1@gmail.com>
2022-01-08 11:18:25 -0300

    Serviços sendo puxado do banco

--- app/Controller/Servico.php
+++ app/Controller/Servico.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Utils\View;
-use \App\Model\Entity\Servico as EntityTestimony;
+use \App\Model\Entity\Servico as EntityServico;
 use \WilliamCosta\DatabaseManager\Pagination;
 
 class Servico{
@@ -16,7 +16,35 @@ class Servico{
     */
     private static function getServicoItems($request, &$obPagination){
 
-        return 'item';
+        //SERVIÇOS
+        $itens = '';
+
+        //QUANTIDADE TOTAL DE REGISTROS
+        $quantidadeTotal = EntityServico::getServicos(null, null, null, 'COUNT(*) AS qtd')->fetchObject()->qtd;
+
+        //PAGINA ATUAL
+        $queryParams = $request->getQueryParams();
+        $paginaAtual = $queryParams['page'] ?? 1;
+
+        //INSTACIA DE PAGINAÇÃO
+        $obPagination = new Pagination($quantidadeTotal, $paginaAtual, 3);
+
+        //RESULTADO QUE SERÃO RENDERIZADOS
+        $result = EntityServico::getServicos(null, 'servicoCod DESC', $obPagination->getLimit());
+
+        //RENDERIZA OS ITENS
+        while($obServico = $result->fetchObject(EntityServico::class)){
+
+            //VIEW DE SERVICO
+            $itens .= View::render('pages/home/servicos/servico-item',[
+                'nome'=> $obServico->servicoNome,
+                'descricao' =>$obServico->servicoDescricao,
+                'icon' =>$obServico->servicoIcon
+            ]);
+        }
+
+        //RETORNA OS ITENS RENDERIZADOS
+        return $itens;
     }
 
     /**
@@ -27,7 +55,9 @@ class Servico{
     public static function getServicos($request){
         
         //VIEW DA SECTION DE SERVICOS
-        return $content = View::render('pages/home/servicos/servicos');
+        return $content = View::render('pages/home/servicos/servicos',[
+            'itens' => self::getServicoItems($request, $obPagination)
+        ]);
     }
 
 }
